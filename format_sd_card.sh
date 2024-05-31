bcefdc2e8de4051070874e0026cfd3681b38daec
Alexandru Ardelean <alex@shruggie.ro>
2024-05-31 15:12:54 +0300

    format_sd_card.sh + populate_sd_card.sh: add utilities for SD-cards
    Signed-off-by: Alexandru Ardelean <alex@shruggie.ro>

---
+++ format_sd_card.sh
@@ -0,0 +1,144 @@
+#!/bin/bash -e
+
+#
+# Usage ./format_sdcard.sh <device-name> <sd-card-device>
+#
+
+contains() {
+	[[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
+}
+
+SUPPORTED_DEVICES="smarc-rzg2l smarc-rzv2l"
+
+usage_check() {
+	if [ -z "$1" ] ; then
+		echo "No device-name specified"
+		echo "Usage: $0 <device-name> <sd-card-device>"
+		exit 1
+	fi
+	if ! contains "$SUPPORTED_DEVICES" "$1" ; then
+		echo "Device '$1' unknown; Supported list is '$SUPPORTED_DEVICES'"
+		echo "Usage: $0 <device-name> <sd-card-device>"
+		exit 1
+	fi
+	if [ -z "$2" ] ; then
+		echo "No SD card device provided"
+		echo "Usage: $0 <device-name> <sd-card-device>"
+		exit 1
+	fi
+	if [ ! -e "$2" ] ; then
+		echo "SD-card '$2' device does not exist"
+		exit 1
+	fi
+	if [[ $2 != /dev/sd* ]] && [[ $2 != /dev/mmcblk* ]] ; then
+		echo "SD-card '$2' device must be named '/dev/sdX' or '/dev/mmcblkX'"
+		exit 1
+	fi
+}
+
+usage_check "$1" "$2"
+
+# Tune these as needed
+SZ_SUFFIX=G                 # G=1024*3 ; GB=1000*3 (choose your favorite)
+FAT_SIZE=256M
+EXT_TYPE=ext4
+FAT_PART_LABEL=RZ_FAT
+ROOTFS_PART_LABEL=RZ_ext
+
+format_partition() {
+	DEV="$1"
+	TYPE="$2"
+	PART_LABEL="$3"
+
+	sudo dd if=/dev/zero of=${DEV} bs=1M count=1
+
+	# format as ext3/ext4
+	if [ "$TYPE" = "vfat" ] ; then
+		sudo mkfs.vfat -F 32 -n $PART_LABEL $DEV
+	else
+		sudo mkfs.${TYPE} -O ^metadata_csum,^64bit,^orphan_file  -L $PART_LABEL $DEV
+	fi
+}
+
+format_sd_card_single_rootfs() {
+	local devname="$1"
+	local PSUF
+
+	for dev in ${devname}* ; do
+		sudo umount $dev &> /dev/null || true
+	done
+
+	echo "== Destroying Master Boot Record =="
+	sleep 1
+	sudo dd if=/dev/zero of=${devname} bs=1M count=1
+
+	echo "== Writing new partition layout =="
+	sleep 1
+	# Create 1 rootfs partition (with the entire disk)
+	echo -e "n\np\n1\n\n\n" \
+		"t\n83\n" \
+		"p\nw\n" | sudo fdisk -u $devname
+
+	sudo sync
+
+	if [[ $devname == /dev/mmcblk* ]] ; then
+		PSUF=p
+	fi
+
+	echo "== Formatting partitions =="
+	sleep 1
+	format_partition ${devname}${PSUF}1 ${EXT_TYPE} $ROOTFS_PART_LABEL
+
+	sudo sync
+}
+
+format_sd_card_fat_and_rootfs() {
+	local devname="$1"
+	local PSUF
+
+	for dev in ${devname}* ; do
+		sudo umount $dev &> /dev/null || true
+	done
+
+	echo "== Destroying Master Boot Record =="
+	sleep 1
+	sudo dd if=/dev/zero of=${devname} bs=1M count=1
+
+	echo "== Writing new partition layout =="
+	sleep 1
+	# Create 1 FAT partition and 1 rootfs partition (with remainder of disk)
+	echo -e "n\np\n1\n\n+${FAT_SIZE}\n" \
+		"n\np\n2\n\n\n" \
+		"t\n1\n6\n" \
+		"p\nw\n" | sudo fdisk -u $devname
+
+	sudo sync
+
+	if [[ $devname == /dev/mmcblk* ]] ; then
+		PSUF=p
+	fi
+
+	echo "== Formatting partitions =="
+	sleep 1
+	format_partition ${devname}${PSUF}1 vfat $FAT_PART_LABEL
+	format_partition ${devname}${PSUF}2 ${EXT_TYPE} $ROOTFS_PART_LABEL
+
+	sudo sync
+}
+
+echo "=================================================================="
+echo "| WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! |"
+echo "|                                                                |"
+echo "| This will destroy all data on '$1'"
+echo "| Press Enter to continue.........                               |"
+echo "|                                                                |"
+echo "| WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! |"
+echo "=================================================================="
+read ans
+
+DEVICE="$1"
+if [ "$DEVICE" = "smarc-rzg2l" ] ; then
+	format_sd_card_fat_and_rootfs "$2"
+elif [ "$DEVICE" = "smarc-rzv2l" ] ; then
+	format_sd_card_fat_and_rootfs "$2"
+fi
---
+++ populate_sd_card.sh
@@ -0,0 +1,165 @@
+#!/bin/bash -e
+
+#
+# Usage ./populate_sdcard.sh <device-name> <sd-card-device> [Yocto-Deploy-Dir]
+#
+
+contains() {
+	[[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && return 0 || return 1
+}
+
+SUPPORTED_DEVICES="smarc-rzg2l smarc-rzv2l"
+
+usage_check() {
+	if [ -z "$1" ] ; then
+		echo "No device-name specified"
+		echo "Usage: $0 <device-name> <sd-card-device> [Yocto-Deploy-Dir - defaults to '$DEFAULT_YOCTO_DIR']"
+		exit 1
+	fi
+	if ! contains "$SUPPORTED_DEVICES" "$1" ; then
+		echo "Device '$1' unknown; Supported list is '$SUPPORTED_DEVICES'"
+		echo "Usage: $0 <device-name> <sd-card-device> [Yocto-Deploy-Dir - defaults to '$DEFAULT_YOCTO_DIR']"
+		exit 1
+	fi
+	if [ -z "$2" ] ; then
+		echo "No SD card device provided"
+		echo "Usage: $0 <device-name> <sd-card-device> [Yocto-Deploy-Dir - defaults to '$DEFAULT_YOCTO_DIR']"
+		exit 1
+	fi
+	if [ ! -e "$2" ] ; then
+		echo "SD-card '$2' device does not exist"
+		exit 1
+	fi
+	if [[ $2 != /dev/sd* ]] && [[ $2 != /dev/mmcblk* ]] ; then
+		echo "SD-card '$2' device must be named '/dev/sdX' or '/dev/mmcblkX'"
+		exit 1
+	fi
+	if [ ! -d "$3" ] ; then
+		echo "Yocto deploy directory '$3' does not exist"
+		exit 1
+	fi
+}
+
+DEVICE="$1"
+DEFAULT_YOCTO_DIR="build/tmp/deploy/images/$DEVICE"
+YOCTO_DEPLOY_DIR="${3:-${DEFAULT_YOCTO_DIR}}"
+
+usage_check "$1" "$2" "$YOCTO_DEPLOY_DIR"
+
+WESTON_ROOTFS_IMG_FILE="$YOCTO_DEPLOY_DIR/core-image-weston-$DEVICE.rootfs.tar.bz2"
+BSP_ROOTFS_IMG_FILE="$YOCTO_DEPLOY_DIR/core-image-bsp-$DEVICE.rootfs.tar.bz2"
+
+copy_boot_files() {
+	local bootdir="$1"
+	local dst="$2"
+
+	sudo rm -rf ${dst}/*
+	for file in ${bootdir}/* ; do
+		echo "Copying '$file'"
+		sudo cp $file ${dst}/
+	done
+}
+
+untar_roofs() {
+	local dst="$1"
+	local rootfs_img_file
+
+	if [ -f "$WESTON_ROOTFS_IMG_FILE" ] ; then
+		rootfs_img_file="$WESTON_ROOTFS_IMG_FILE"
+	else
+		rootfs_img_file="$BSP_ROOTFS_IMG_FILE"
+	fi
+
+	echo "Unpacking rootfs file '$rootfs_img_file'"
+
+	sudo rm -rf ${dst}/*
+	sudo tar -xf "$rootfs_img_file" -C "$dst"
+}
+
+populate_sd_card_single_rootfs() {
+	local devname="$1"
+	local PSUF
+	local TMP="/tmp"
+
+	local mount_dir1="${TMP}/mount_work1/"
+
+	mkdir -p ${mount_dir1}
+
+	if [[ $devname == /dev/mmcblk* ]] ; then
+		PSUF=p
+	fi
+
+	echo == Unmounting partitions first ==
+	sudo umount ${devname}${PSUF}1 &> /dev/null || true
+
+	# Populate rootfs
+	echo "== Populating rootfs partition '${devname}${PSUF}1' =="
+	sudo mount ${devname}${PSUF}1 ${mount_dir1}
+	untar_roofs "${mount_dir1}"
+
+	echo "== Syncing... =="
+
+	sudo sync
+
+	echo == Unmounting partitions \(almost done\) ==
+	sudo umount ${devname}${PSUF}1
+
+	echo "== Done... =="
+}
+
+populate_sd_card_rootfs_and_fat() {
+	local devname="$1"
+	local PSUF
+	local TMP="/tmp"
+
+	local mount_dir1="${TMP}/mount_work1/"
+	local mount_dir2="${TMP}/mount_work2/"
+
+	mkdir -p ${mount_dir1}
+	mkdir -p ${mount_dir2}
+
+	if [[ $devname == /dev/mmcblk* ]] ; then
+		PSUF=p
+	fi
+
+	echo == Unmounting partitions first ==
+	sudo umount ${devname}${PSUF}1 &> /dev/null || true
+	sudo umount ${devname}${PSUF}2 &> /dev/null || true
+
+	# Populate rootfs
+	echo "== Populating rootfs partition '${devname}${PSUF}2' =="
+	sudo mount ${devname}${PSUF}2 ${mount_dir2}
+	untar_roofs "${mount_dir2}"
+
+	echo "== Populating boot partition '${devname}${PSUF}1' with files from '${mount_dir2}/boot' =="
+	# Populate FAT
+	sudo mount ${devname}${PSUF}1 ${mount_dir1}
+	copy_boot_files "${mount_dir2}/boot" "${mount_dir1}"
+
+	echo "== Syncing... =="
+
+	sudo sync
+
+	echo == Unmounting partitions \(almost done\) ==
+	sudo umount ${devname}${PSUF}2
+	sudo umount ${devname}${PSUF}1
+
+	echo "== Done... =="
+}
+
+echo "=================================================================="
+echo "| WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! |"
+echo "|                                                                |"
+echo "| This will erase all files on partitions of '$1'"
+echo "| Press Enter to continue.........                               |"
+echo "|                                                                |"
+echo "| WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! |"
+echo "=================================================================="
+read ans
+
+DEVICE="$1"
+if [ "$DEVICE" = "smarc-rzg2l" ] ; then
+	populate_sd_card_rootfs_and_fat "$2"
+elif [ "$DEVICE" = "smarc-rzv2l" ] ; then
+	populate_sd_card_rootfs_and_fat "$2"
+fi
